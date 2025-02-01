06c9f2226ddf1607994eae948fdd22a21f6a44e8
Jester Lumacad <mr.jesterlumacad@gmail.com>
2025-02-01 12:58:06 +0800

    refined version outputs

--- cmd/checkversions.sh
+++ cmd/checkversions.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
-# fastfetch --config examples/8 && \
-# echo -e "\n🐍 Python Version:" && python --version && \
-# echo -e "\n🦫 Go Version:" && go version && \
-# echo -e "\n📘 TypeScript Version:" && tsc --version && \
-# echo -e "\n🦀 Rust Version:" && rustc --version && \
-# echo -e "\n📦 Conda Version:" && conda --version && \
-# echo -e "\n🦕 Deno Version:" && deno --version && \
-# echo -e "\n🌐 Node Version:" && node --version && \
-# echo -e "\n📦 Cargo Version:" && cargo --version && \
-# echo -e "\n🐳 Docker Version:" && docker --version && \
-# echo -e "\n🪐 Neovim Version:" && nvim --version && \
-# echo -e "\n@MRJXTR - CHECK VERSIONS: DONE"
-
 echo -e "\nCHECKING DEV TOOLS/STACK VERSIONS\n\n"
 
 fastfetch --config examples/8
 
-echo -e "\n\n🐍 Python: $(python --version | awk '{print $2}')"
-echo -e "🦫 Go: $(go version | awk '{print $3}' | cut -c3-)"
-echo -e "📘 TypeScript: $(tsc --version | awk '{print $2}')"
-echo -e "🦀 Rust: $(rustc --version | awk '{print $2}')"
-echo -e "📦 Conda: $(conda --version | awk '{print $2}')"
-echo -e "🦕 Deno: $(deno --version | head -n1 | awk '{print $2}')"
-echo -e "🌐 Node: $(node --version | cut -c2-)"
-echo -e "📦 Cargo: $(cargo --version | awk '{print $2}')"
-echo -e "🐳 Docker: $(docker --version | awk '{print $3}' | tr -d ',')"
-echo -e "🪐 Neovim: $(nvim --version | head -n1 | awk '{print $2}')"
+echo -e "\n\n🐍 Python: v$(python --version | awk '{print $2}')"
+echo -e "🦫 Go: v$(go version | awk '{print $3}' | cut -c3-)"
+echo -e "📘 TypeScript: v$(tsc --version | awk '{print $2}')"
+echo -e "🦀 Rust: v$(rustc --version | awk '{print $2}')"
+echo -e "📦 Conda: v$(conda --version | awk '{print $2}')"
+echo -e "🦕 Deno: v$(deno --version | head -n1 | awk '{print $2}')"
+echo -e "🌐 Node: v$(node --version | cut -c2-)"
+echo -e "📦 Cargo: v$(cargo --version | awk '{print $2}')"
+echo -e "🐳 Docker: v$(docker --version | awk '{print $3}' | tr -d ',')"
+echo -e "🪐 Neovim: v$(nvim --version | head -n1 | awk '{print $2}' | cut -c2-)"
